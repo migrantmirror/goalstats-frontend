88220bc818b3e9bc2a424f6361f2666236008f40
migrantmirror <onyekachiebosi@gmail.com>
2025-06-12 04:11:14 +0400

    Remove package-lock.json and update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import './index.css';
 import './index.css';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
